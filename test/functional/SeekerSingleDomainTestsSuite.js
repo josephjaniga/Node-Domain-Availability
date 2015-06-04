8452ba3bf4a802279f4f40d547f886a1fed43418
Joe Janiga <josephjaniga@gmail.com>
2015-06-04 08:29:32 -0400

    fixed the Single Domain Specs

--- test/functional/SeekerSingleDomainTestsSuite.js
+++ test/functional/SeekerSingleDomainTestsSuite.js
@@ -3,40 +3,38 @@ var Seeker = require('./../../dist/Seeker.js'),
 
 process.setMaxListeners(0);
 
-describe("Seeker - Single Domain Tests: ", function(done){
+describe("Seeker - Single Domain Tests: ", function(){
 
     var s = new Seeker();
 
     // negative tests... these domains should not be available
     it("Travis-CI.org availability promise should return false", function(){
-        return s.isAvailable("travis-ci.org").should.eventually.equal(false);
+        s.isAvailable("travis-ci.org").should.eventually.equal(false);
     });
     
     it("USA.gov availability promise should return false", function(){
-        return s.isAvailable("usa.gov").should.eventually.equal(false);
+        s.isAvailable("usa.gov").should.eventually.equal(false);
     });
     
     it("Google.com availability promise should return false", function(){
-        this.timeout(5000);
-        return s.isAvailable("google.com").should.eventually.equal(false);
+        s.isAvailable("google.com").should.eventually.equal(false);
     });
 
     it("Comcast.net availability promise should return false", function(){
-        this.timeout(5000);
-        return s.isAvailable("comcast.net").should.eventually.equal(false);
+        s.isAvailable("comcast.net").should.eventually.equal(false);
     });
 
     // positive tests... these domains should be available
     it("RedHotLizardCaltrops.ninja availability promise should return true", function(){
-        return s.isAvailable("RedHotLizardCaltrops.ninja").should.eventually.equal(true);
+        s.isAvailable("RedHotLizardCaltrops.ninja").should.eventually.equal(true);
     });
 
     it("SomeKindOfLazerChicken.travel availability promise should return true", function(){
-        return s.isAvailable("SomeKindOfLazerChicken.travel").should.eventually.equal(true);
+        s.isAvailable("SomeKindOfLazerChicken.travel").should.eventually.equal(true);
     });
 
     it("AnAppleADayIsLikeAlotOfFruit.agency availability promise should return true", function(){
-        return s.isAvailable("AnAppleADayIsLikeAlotOfFruit.agency").should.eventually.equal(true);
+        s.isAvailable("AnAppleADayIsLikeAlotOfFruit.agency").should.eventually.equal(true);
     });
     
 });
